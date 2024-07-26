0f01e11cefc4cf8eb3f7f590d359ca4193bbe720
conlibra <114357265+conlibra@users.noreply.github.com>
2024-07-25 23:00:51 -0700

    Update foodRender.js

--- scripts/foodRender.js
+++ scripts/foodRender.js
@@ -1,47 +1,5 @@
+import { renderImages } from "./common.js";
+
 const numberOfFoodPhotos = 45;
 
-const photoGrid = document.getElementById("photo-grid");
-const loader = document.getElementById("loader");
-// Flag to see if fist 4 images loaded to be able to remove a loader
-let firstImagesLoaded = false;
-
-function renderImages() {
-  // Staring to show loader when rendering images
-  addImageProps(numberOfFoodPhotos);
-  const imgs = getFist4Images();
-
-  const interval = setInterval(() => {
-    if (firstImagesLoaded === false) {
-      firstImagesLoaded = imgs.every((img) => img.naturalHeight !== 0);
-    } else {
-      loader.style.display = "none";
-      clearInterval(interval);
-    }
-  }, 100);
-}
-
-function addImageProps(numOfImages) {
-  for (let i = numOfImages; i >= 1; i -= 1) {
-    const img = document.createElement("img");
-    img.setAttribute("src", `./assets/gallery/food/${i}.jpg`);
-    img.setAttribute("class", "gallery-item");
-    img.setAttribute("id", i);
-    img.setAttribute(
-      "alt",
-      `Seattle, Washington food photography item ${i} of ${numOfImages}`
-    );
-    photoGrid.appendChild(img);
-  }
-}
-
-function getFist4Images() {
-  const imgs = [];
-  for (let i = 1; i < 4; i += 1) {
-    const imgElement = document.getElementById(i);
-    imgs.push(imgElement);
-  }
-
-  return imgs;
-}
-
-renderImages();
+renderImages(numberOfFoodPhotos, "food");
